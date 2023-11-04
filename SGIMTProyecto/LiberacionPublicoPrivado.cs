9b89b2f1e058d003ce36412f2e7594749caaa355
LuisPiedras7 <144396908+LuisPiedras7@users.noreply.github.com>
2023-11-03 22:30:57 -0600

    Pantallas

---
+++ SGIMTProyecto/LiberacionPublicoPrivado.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SGIMTProyecto
+{
+    public partial class LiberacionPublicoPrivado : UserControl
+    {
+        public LiberacionPublicoPrivado()
+        {
+            InitializeComponent();
+        }
+
+        private void GRB_Liberacion_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}

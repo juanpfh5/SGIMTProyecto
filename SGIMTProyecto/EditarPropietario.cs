5828fbcd9e1f809c843db7ffa0c7f6567ae67eec
LuisPiedras7 <144396908+LuisPiedras7@users.noreply.github.com>
2023-11-03 22:30:57 -0600

    Pantallas

---
+++ SGIMTProyecto/EditarPropietario.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SGIMTProyecto
+{
+    public partial class EditarPropietario : UserControl
+    {
+        public EditarPropietario()
+        {
+            InitializeComponent();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void JBL_NoConcesion_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
